eaf7d41a43895e701b53c3d34e1f470a517fe7ae
fernandor-p <fernandor.p@hotmail.com>
2021-07-02 21:54:33 -0400

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route , Switch} from 'react-router-dom'
+import Home from './views/home'
+import Error404 from './views/error404'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App= ()=>{
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/home">
+                    <Home />
+                </Route>
+                <Route>
+                    <Error404 />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ src/store/flux.js
@@ -0,0 +1,15 @@
+// import { useReducer, useState } from "react";
+
+
+// const getState=({getStore, getActions, setStore})=>{
+
+//     return {
+//         store:{
+
+//         },
+//         actions:{
+//         }
+//     }
+// }
+
+// export default getState;
